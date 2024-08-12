719b977f935bd7b72af056cb68cee1b5bc75a404
saymahtab <reachmahtabsce@gmail.com>
2024-08-12 15:02:29 +0530

    update js

--- script.js
+++ script.js
@@ -8,22 +8,26 @@ async function fetchQuestions(){
     try {
         const resp = await fetch("https://opentdb.com/api.php?amount=10");
         if(!resp.ok){
-            throw new Error("Could'nt Fetch qstions!");
+            throw new Error("Couldn't Fetch questions!");
         }
         const data = await resp.json();
         Questions = data.results;   
+        loadQues();  // Load the first question after fetching
     }
     catch(err){
         console.error(err);
-        ques.innerHTML = `<h5 style="background-color:"red">${err}</h5>`;
+        quesTag.innerHTML = `<h">${err}</h5>`;
     }
 }
 
-if(Questions.length == 0){
-    ques.innerHTML = ` <h5 style="background-color:"red"">Please Wait Questions are loading....</h5>`;
+if(Questions.length === 0){
+    quesTag.innerHTML = `<h5>Please Wait, Questions are loading....</h5>`;
 }
 
 function loadQues() {
+    if (Questions.length === 0) {
+        return;
+    }
 
     const opt = document.getElementById('opt');
 
@@ -39,18 +43,16 @@ function loadQues() {
 
     options.sort(() => Math.random() - 0.5);
 
-    options.forEach((option ) => {
+    options.forEach((option) => {
         const optDiv = document.createElement('div');
         const optionTag = document.createElement('input');
         const labelTag = document.createElement('label');
 
         optionTag.type = 'radio';
-        // optionTag.name = option${idx};
         optionTag.name = "answer";
         optionTag.value = option;
 
         labelTag.textContent = option;
-        // labelTag.htmlFor = option${idx};
 
         optDiv.appendChild(optionTag);
         optDiv.appendChild(labelTag);
@@ -59,28 +61,22 @@ function loadQues() {
     });
 }
 
-
-setTimeout(() => {
-    loadQues();
-    if(Questions.length == 0){
-        ques.innerHTML = ` <h5 style="background-color:"yellow"">Wait for the content to load / Please try  Again!!</h5>`
-    }
-} ,2000);
-
-
-
 function checkAnswer(){
-   const selectAns = document.querySelector(
-    `input[name="answer"]:checked`
-   ).value;
-   if(selectAns === Questions[currentQuestion].correct_answer){
-    score++;
-   }
-   nextQuestion();
+    const selectedAns = document.querySelector(`input[name="answer"]:checked`);
+    if (!selectedAns) {
+        alert("Please select an answer!");
+        return;
+    }
 
+    const selectAnsValue = selectedAns.value;
+    if(selectAnsValue === Questions[currentQuestion].correct_answer){
+        score++;
+    }
+    nextQuestion();
 }
+
 function nextQuestion(){
-    if(currentQuestion < Questions.length -1){
+    if(currentQuestion < Questions.length - 1){
         currentQuestion++;
         loadQues();
     } else{
@@ -89,18 +85,13 @@ function nextQuestion(){
         document.getElementById("btn").remove();
         showTotal();
     }  
-
 }
 
 function showTotal(){
     const totalScore = document.querySelector('#score');
-    totalScore.innerHTML = `<h3>Your Score :${score}/10</h3>`;
-    Questions.forEach(( ques , indx ) => {
-       totalScore.innerHTML +=`
-       <p>
-       ${indx+1 }: ${ ques.correct_answer}
-       </p>
-       `;
+    totalScore.innerHTML = `<h3>Your Score: ${score}/10</h3>`;
+    Questions.forEach((ques, indx) => {
+        totalScore.innerHTML += `<p>${indx + 1}: ${ques.correct_answer}</p>`;
     });
 }
 
